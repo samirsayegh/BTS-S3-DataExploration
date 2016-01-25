686a1c0bfeacd8b57e1c47637614fb9810370ea5
Samir Sayegh <samirsayegh2@gmail.com>
2016-01-25 13:07:47 +0100

    Comments on the queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
-select count(*) from vineitems;
-select distinct vine from vineitems;
-select * from vineitems where mex like '% RT %' OR mex like 'RT %';
-select date(created_at) as date, count(*) as number from vineitems group by date;
-select `vine` as vine, date(`created_at`) as date from vineitems group by vine, date;
-select count(*) as cant, `vine` as vine, date(`created_at`) as date from vineitems group by vine, date;
-select count(*) as cant, dayname(`created_at`) as date from vineitems group by date order by cant desc limit 1;
-select count(*) as cant, vine as date from vineitems group by vine order by cant desc;
+select count(*) from vineitems; -- How many tweets? 
+select distinct vine from vineitems; -- Unique Vines? 
+select * from vineitems where mex like '% RT %' OR mex like 'RT %'; -- Retweets? 
+select date(created_at) as date, count(*) as number from vineitems group by date; -- How many tweets per day? 
+select `vine` as vine, date(`created_at`) as date from vineitems group by vine, date; -- Unique Vine per day?
+select count(*) as cant, `vine` as vine, date(`created_at`) as date from vineitems group by vine, date; -- Unique Vine per day? 
+select count(*) as cant, dayname(`created_at`) as date from vineitems group by date order by cant desc limit 1; -- Day of the week with the highest peak?
+select count(*) as cant, vine as date from vineitems group by vine order by cant desc; -- Which are the most shared Vine? 
